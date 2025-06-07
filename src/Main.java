305def23f869e6adfd70a72e417040a95eb5f59f
vedantpanda <ved07panda@gmail.com>
2025-06-07 21:32:55 +0530

    Stream Important Question

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,10 @@ public class Main {
 
         productCategoryQuantity();
 
+        groupingByExample();
+
+        getSummaryStatistics();
+
     }
 
     private static List<String> stockIssueDetector() {
@@ -51,6 +55,25 @@ public class Main {
 
     private static void productCategoryQuantity() {
 
+        List<Order> orders = getOrders();
+
+        Map<String, Integer> productCategoryQuantityMap = orders.stream()
+                .filter(order -> order.getTotalValue() > 500)
+                .filter(order -> order.getOrderTime().isAfter(LocalDateTime.now().minusHours(24)))
+                .flatMap(order -> order.getProducts().stream())
+                .filter(Product::isInStock)
+                .collect(Collectors.groupingBy(Product::getCategory, Collectors.summingInt(Product::getQuantity)))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (a, b) -> a, LinkedHashMap::new));
+
+        System.out.println(productCategoryQuantityMap);
+
+    }
+
+    private static List<Order> getOrders() {
         List<Product> p1 = Arrays.asList(new Product("1", "iPhone", "Electronics", 2, true, 200),
                 new Product("2", "Usb cable", "Electronics", 5, true, 200),
                 new Product("3", "Head phones", "Electronics", 0, false, 200)
@@ -82,21 +105,31 @@ public class Main {
 
         Order o5 = new Order("3", 700, LocalDateTime.now().minusHours(1), p5);
 
-        List<Order> orders = Arrays.asList(o1, o2, o3, o4, o5);
-
-        Map<String, Integer> productCategoryQuantityMap = orders.stream()
-                .filter(order -> order.getTotalValue() > 500)
-                .filter(order -> order.getOrderTime().isAfter(LocalDateTime.now().minusHours(24)))
-                .flatMap(order -> order.getProducts().stream())
-                .filter(Product::isInStock)
-                .collect(Collectors.groupingBy(Product::getCategory, Collectors.summingInt(Product::getQuantity)))
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
-                        (a, b) -> a, LinkedHashMap::new));
-
-        System.out.println(productCategoryQuantityMap);
+        return Arrays.asList(o1, o2, o3, o4, o5);
 
     }
+
+    private static void groupingByExample() {
+
+        List<Order> orders = getOrders();
+
+        Map<String, List<Product>> categoryProductMap = orders.stream()
+                .flatMap(order -> order.getProducts().stream())
+                .collect(Collectors.groupingBy(Product::getCategory));
+
+        System.out.println(categoryProductMap);
+
+    }
+
+    private static void getSummaryStatistics() {
+        List<Order> orders = getOrders();
+        IntSummaryStatistics summaryStatistics = orders.stream().flatMap(order -> order.getProducts().stream())
+                .mapToInt(Product::getQuantity).summaryStatistics();
+        System.out.println(summaryStatistics.getMax());
+        System.out.println(summaryStatistics.getAverage());
+        System.out.println(summaryStatistics.getCount());
+        System.out.println(summaryStatistics.getMin());
+        System.out.println(summaryStatistics.getSum());
+    }
+
 }
